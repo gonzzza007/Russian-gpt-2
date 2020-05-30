84f2231322f421a5cef606fb5bc53322331831bb
gonzzza <gonzzza@gmail.com>
2020-05-31 00:26:45 +0300

    fixes4

--- src/sample.py
+++ src/sample.py
@@ -44,8 +44,7 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
         context = tf.fill([batch_size, 1], start_token)
 
     def step(hparams, tokens, past=None):
-        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.AUTO_REUSE)
-
+        lm_output = model.model(hparams=hparams, X=tokens, past=past, reuse=tf.compat.v1.AUTO_REUSE)
         logits = lm_output['logits'][:, :, :hparams.n_vocab]
         presents = lm_output['present']
         presents.set_shape(model.past_shape(hparams=hparams, batch_size=batch_size))
